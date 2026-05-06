b9c3b12ae7df95b3eddab5ce44d8a1018a191cf6
hamzi-haidar <hamzihaider@gmail.com>
2026-05-06 21:04:22 +0300

    testing pull request effect in github

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <div>
+      {/* ?added this to test pull request effect on contribution */}
       <Navigation />
       <h1>The Wild Oasis. Welcome to paradise</h1>
       <Link href="/cabins">Explore luxury cabins</Link>
